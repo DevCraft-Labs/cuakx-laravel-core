a1ab768c495364348c6cec3e7375b1748a79e93e
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2026-07-18 13:49:33 +0700

    Hotfix: When request is empty, instead of comparing with [], I force convert it into {}

--- src/Utils/Auth/XToken/XTokenUtil.php
+++ src/Utils/Auth/XToken/XTokenUtil.php
@@ -9,6 +9,11 @@ class XTokenUtil
     public static function validateToken(string $token, Request $request): bool {
         $stringified_request = json_encode($request->all());
 
+        // If request is empty, and json_encode returns '[]', we need to convert it to '{}' to match the expected hash.
+        if($stringified_request == '[]') {
+            $stringified_request = '{}';
+        }
+
         $hash = hash_hmac('sha512', $stringified_request, env('APP_KEY'));
 
         return hash_equals($hash, $token);
